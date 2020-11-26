0b67151ecb186d04f1a301c49a58819c46d32b72
EikoNakashima <68436861+EikoNakashima@users.noreply.github.com>
2020-11-26 21:35:58 +0900

    commit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,24 @@
 class EventsController < ApplicationController
+  
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
 end
