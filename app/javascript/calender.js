985d1b769bf22e4f536701fa7a2ffbe5d5a714f5
EikoNakashima <68436861+EikoNakashima@users.noreply.github.com>
2020-11-28 21:30:15 +0900

    calender-js-new

--- app/javascript/calender.js
+++ app/javascript/calender.js
@@ -1,13 +1,36 @@
+
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 
-document.addEventListener('turbolinks:load', function() {
-  var calendarEl = document.getElementById('calendar');
 
+document.addEventListener('DOMContentLoaded', function() {
+  
+  var calendarEl = document.getElementById('calendar');
   var calendar = new Calendar(calendarEl, {
+    locale: 'ja',
+    height: 'auto',
+    headerToolbar: {
+      left: "dayGridMonth,listMonth",
+      center: "title",
+      right: "today prev,next"
+    },
+    buttonText: {
+      today: '今月',
+      month: '月',
+      list: 'リスト',
+    },
+    dayCellContent: function(e) {
+      e.dayNumberText = e.dayNumberText.replace('日', '');
+    },
     plugins: [ dayGridPlugin, interactionPlugin ]
   });
 
   calendar.render();
 });
+
+document.getElementById("confirm").addEventListener('click', function()　{
+  calendar.refetchEvents();
+  calendar.render();
+});
+
