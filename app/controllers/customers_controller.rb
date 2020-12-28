9716c7268ee03f67862fd2501ab3cc6f344e0543
EikoNakashima <68436861+EikoNakashima@users.noreply.github.com>
2020-12-29 00:24:56 +0900

    tubolinks:load_fix
    トップページに戻る際にfullcalenderが動くように修正した

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -18,7 +18,7 @@ class CustomersController < ApplicationController
       redirect_to root_path, notice: "顧客を登録しました"
     else
       redirect_to root_path, alert: "登録出来ませんでした"
-    # @events = Event.where(user_id: current_user.id)
+   
     end
   end
 
@@ -26,13 +26,9 @@ class CustomersController < ApplicationController
     if @customer.destroy
       redirect_to root_path, notice: '顧客を削除しました'
     else
-      flash.now[:alert] = '顧客を削除できませんでした'
+      flash.now[:alert] = '削除できませんでした'
       render :show
     end
-    # @user = User.find(params[:id])
-    # event = Event.find(params[:id])
-    # event.destroy
-    # redirect_to user_path(@user)
   end
 
   def edit
@@ -40,7 +36,7 @@ class CustomersController < ApplicationController
 
   def update
     if @customer.update(customer_params)
-      redirect_to customers_path, notice: '商品を編集しました'
+      redirect_to root_path, notice: '顧客情報を編集しました'
     else
       flash.now[:alert] = '必須事項を入力してください'
       render :edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :events
-  has_many :customers
+  has_many :events,foreign_key: :user_id, dependent: :destroy
+  has_many :customers,foreign_key: :user_id, dependent: :destroy
 end
