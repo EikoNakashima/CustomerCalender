7b0554b3552b4d83f2c057e95d0a90f7dcff6a41
EikoNakashima <68436861+EikoNakashima@users.noreply.github.com>
2021-01-12 18:48:45 +0900

    logout-fix2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!,except: [:index]
 
   def after_sign_in_path_for(resource)
-    mypage_root_path 
+    root_path 
   end
 
   def after_sign_out_path_for(resource)
